87b405f7d9a95292d4d233dfa902586c5190cac6
Al Amin <alamin>
2023-02-24 21:09:01 +0600

    tailwind

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("secondBtn").addEventListener("click",() => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href="">Why do I have this issue?</a>'
+      })
+})
